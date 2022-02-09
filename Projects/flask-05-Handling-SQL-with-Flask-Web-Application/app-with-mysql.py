881246182850f4260f19094811ba42f385b4d798
HRNGR <gurharun@gmail.com>
2022-02-09 12:43:18 +0300

    lesson

--- Projects/flask-05-Handling-SQL-with-Flask-Web-Application/app-with-mysql.py
+++ Projects/flask-05-Handling-SQL-with-Flask-Web-Application/app-with-mysql.py
@@ -6,7 +6,7 @@ from flaskext.mysql import MySQL
 app = Flask(__name__)
 
 # Configure mysql database
-app.config['MYSQL_DATABASE_HOST'] = 'harun-database-1.chzbvsbsj0l3.us-east-1.rds.amazonaws.com'
+app.config['MYSQL_DATABASE_HOST'] = 'harun.chzbvsbsj0l3.us-east-1.rds.amazonaws.com'
 app.config['MYSQL_DATABASE_USER'] = 'admin'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'Clarusway_1'
 app.config['MYSQL_DATABASE_DB'] = 'clarusway'
@@ -108,4 +108,4 @@ def add_email():
 # Add a statement to run the Flask application which can be reached from any host on port 80.
 if __name__ == '__main__':
    app.run(debug=True)
-   # app.run(host='0.0.0.0', port=80)
+   # app.run("0.0.0.0", port=80)
