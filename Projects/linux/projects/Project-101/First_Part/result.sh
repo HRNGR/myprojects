71b3aa9213aee3d3ba9fa453e844d91c674c024d
HRNGR <gurharun@gmail.com>
2022-02-15 23:36:19 +0300

    projectlinux

--- Projects/linux/projects/Project-101/First_Part/result.sh
+++ Projects/linux/projects/Project-101/First_Part/result.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
-cat event_history.csv | grep -i "serdar" | grep -i "terminateinstance" |  awk -F, '{print $1,$4,$10}' | tee result.txt
+
+echo -e "Bu script *nightfighter* AWS-DevOps Grubu tarafindan yapilmiştir.\n"
+
+cat event_history.csv | grep -i "serdar" | grep -i "terminateinstance"  | awk -F'"' '{print $16}' | cat >> result.txt
+
+cat event_history.csv | grep -i "serdar" | grep -i "terminateinstance"  | awk -F'"' '{print $32}' | cat >> result.txt
+
+cat event_history.csv | grep -i "serdar" | grep -i "terminateinstance"  | awk -F'"' '{print $48}' | cat >> result.txt
+
+cat event_history.csv | grep -i "serdar" | grep -i "terminateinstance"  | awk -F'"' '{print $64}' | cat >> result.txt
+
+cat event_history.csv | grep -i "serdar" | grep -i "terminateinstance"  | awk -F'"' '{print $80}' | cat >> result.txt
+
+cat event_history.csv | grep -i "serdar" | grep -i "terminateinstance"  | awk -F'"' '{print $96}' | cat >> result.txt
+
+sort result.txt | uniq | tee result.txt
