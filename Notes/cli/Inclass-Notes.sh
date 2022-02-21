9a8ed474905d6766794ca6e420b5e450d53aa696
HRNGR <gurharun@gmail.com>
2022-02-22 01:16:33 +0300

    After Lesson

--- Notes/cli/Inclass-Notes.sh
+++ Notes/cli/Inclass-Notes.sh
@@ -3,15 +3,12 @@
 
 # References
 # https://docs.aws.amazon.com/cli/latest/userguide/cli-chap-welcome.html
-# https://awscli.amazonaws.com/v2/documentation/api/latest/index.html
+### https://awscli.amazonaws.com/v2/documentation/api/latest/index.html
 # https://aws.amazon.com/blogs/compute/query-for-the-latest-amazon-linux-ami-ids-using-aws-systems-manager-parameter-store/
-
-## https://docs.aws.amazon.com/cli/latest/userguide/getting-started-install.html
-## https://awscli.amazonaws.com/v2/documentation/api/latest/index.html
 
 
 # Installation
-
+###
 # https://docs.aws.amazon.com/cli/latest/userguide/getting-started-install.html
 
 
@@ -24,9 +21,9 @@
 # https://graspingtech.com/install-and-configure-aws-cli/
 
 
+
 # Linux:
 # https://docs.aws.amazon.com/cli/latest/userguide/getting-started-install.html
-
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip  #install "unzip" if not installed
 sudo ./aws/install
@@ -37,7 +34,7 @@ sudo yum install awscli -y
 
 aws configure
 
-#ls -al and see .aws folder
+#ls -al than see ".aws" folder
 #cd .aws
 #cat config
 #cat credentials
@@ -46,6 +43,9 @@ aws configure
 
 cat .aws/config
 cat .aws/credentials
+
+aws configure list-profiles
+
 
 aws configure --profile user1
 
@@ -56,8 +56,10 @@ aws configure list-profiles
 
 aws sts get-caller-identity #cd
 
-    ##look consol IAM 
-    ##search for "aws cli reference" attantion for V2 https://docs.aws.amazon.com/cli/latest/index.html
+    ##look at aws consol IAM 
+    ##search for "aws cli reference" attantion for V2 
+    https://docs.aws.amazon.com/cli/latest/index.html
+
     ##aws help
     ##aws <command> help ==>>> aws s3 help
     ##aws <command> <subcommand> help ==>>> aws s3 mb help
@@ -88,35 +90,48 @@ aws s3 rb s3://guile-cli-bucket        !!!!
 
 # EC2
 aws ec2 describe-instances
-##https://awscli.amazonaws.com/v2/documentation/api/latest/reference/ec2/describe-instances.html
+###https://awscli.amazonaws.com/v2/documentation/api/latest/reference/ec2/describe-instances.html
+###https://docs.aws.amazon.com/cli/latest/userguide/cli-services-ec2-sg.html#creating-a-security-group
 
-
+##
 aws ec2 run-instances \
    --image-id ami-033b95fb8079dc481 \
    --count 1 \
    --instance-type t2.micro \
    --key-name KEY_NAME_HERE # put your key name 
+##
+
+
 
 aws ec2 describe-instances \
    --filters "Name = key-name, Values = KEY_NAME_HERE" # put your key name
 
 ## https://awscli.amazonaws.com/v2/documentation/api/latest/reference/ec2/describe-instances.html
 
-## --dıy-run 
+## --dry-run 
 ## --dry-run | --no-dry-run (boolean)
+aws ec2 describe-instances --dry-run
+An error occurred (DryRunOperation) when calling the Descrces operation: Request would have succeeded, but DryRun fl
+
 ## Checks whether you have the required permissions for the action, without actually making the request, and provides an error response. If you have the required permissions, the error response is DryRunOperation . Otherwise, it is UnauthorizedOperation .
 
-
+##Query
 aws ec2 describe-instances --query "Reservations[].Instances[].PublicIpAddress[]"
 
+      ##Filter
 aws ec2 describe-instances \
    --filters "Name = key-name, Values = KEY_NAME_HERE" --query "Reservations[].Instances[].PublicIpAddress[]" # put your key name
 
+
+
+##
+aws ec2 describe-instances \
+ --filters "Key Name = firstkey"
+##
+
+
 aws ec2 describe-instances \
    --filters "Name = instance-type, Values = t2.micro" --query "Reservations[].Instances[].InstanceId[]"
-
-
-
 
 
 aws ec2 stop-instances --instance-ids INSTANCE_ID_HERE # put your instance id
@@ -129,18 +144,20 @@ aws ssm get-parameters --names /aws/service/ami-amazon-linux-latest/amzn2-ami-hv
 
 aws ssm get-parameters --names /aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2 --query 'Parameters[0].[Value]' --output text
 
-aws ec2 run-instances \
-   --image-id $(aws ssm get-parameters --names /aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2 --query 
-'Parameters[0].[Value]' --output text) \
-   --count 1 \
-   --instance-type t2.micro
+###
+https://docs.aws.amazon.com/systems-manager/latest/userguide/sysman-paramstore-access.html
+https://docs.aws.amazon.com/cli/latest/reference/ssm/get-parameters.html
+
+aws ssm get-parameters \
+    --names "MyStringParameter" "MyStringListParameter" "MyInvalidParameterName"
+
+aws ssm get-parameters \
+    --names MyStringParameter MyStringListParameter \
+    --query "Parameters[*].{Name:Name,Value:Value}"
 
 
-##
-aws ec2 run-instances \
-   --image-id ami-033b95fb8079dc481 \
-   --count 1 \
-   --instance-type t2.micro
+###
+
 
 
 
@@ -157,6 +174,14 @@ sudo ./aws/install
 # Update the path accordingly if needed
 export PATH=$PATH:/usr/local/bin/aws
 
+
+
+
+
+
+## Derssonu ilaveler 
+
+https://www.bluematador.com/learn/aws-cli-cheatsheet
 
 
 
