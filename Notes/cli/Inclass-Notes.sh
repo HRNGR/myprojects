48da722150b80de304d4f0858168a8ff72778548
HRNGR <gurharun@gmail.com>
2022-02-22 00:23:29 +0300

    After Lesson

---
+++ Notes/cli/Inclass-Notes.sh
@@ -0,0 +1,163 @@
+# AWS CLI
+# Guile - 02_21_2022
+
+# References
+# https://docs.aws.amazon.com/cli/latest/userguide/cli-chap-welcome.html
+# https://awscli.amazonaws.com/v2/documentation/api/latest/index.html
+# https://aws.amazon.com/blogs/compute/query-for-the-latest-amazon-linux-ami-ids-using-aws-systems-manager-parameter-store/
+
+## https://docs.aws.amazon.com/cli/latest/userguide/getting-started-install.html
+## https://awscli.amazonaws.com/v2/documentation/api/latest/index.html
+
+
+# Installation
+
+# https://docs.aws.amazon.com/cli/latest/userguide/getting-started-install.html
+
+
+# Win:
+# https://docs.aws.amazon.com/cli/latest/userguide/getting-started-install.html
+
+
+# Mac:
+# https://docs.aws.amazon.com/cli/latest/userguide/getting-started-install.html
+# https://graspingtech.com/install-and-configure-aws-cli/
+
+
+# Linux:
+# https://docs.aws.amazon.com/cli/latest/userguide/getting-started-install.html
+
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip  #install "unzip" if not installed
+sudo ./aws/install
+sudo yum install awscli -y
+
+
+# Configuration
+
+aws configure
+
+#ls -al and see .aws folder
+#cd .aws
+#cat config
+#cat credentials
+
+
+
+cat .aws/config
+cat .aws/credentials
+
+aws configure --profile user1
+
+export AWS_PROFILE=user1
+export AWS_PROFILE=default
+
+aws configure list-profiles
+
+aws sts get-caller-identity #cd
+
+    ##look consol IAM 
+    ##search for "aws cli reference" attantion for V2 https://docs.aws.amazon.com/cli/latest/index.html
+    ##aws help
+    ##aws <command> help ==>>> aws s3 help
+    ##aws <command> <subcommand> help ==>>> aws s3 mb help
+
+
+
+
+# IAM
+aws iam list-users
+
+aws iam create-user --user-name aws-cli-user
+
+aws iam delete-user --user-name aws-cli-user
+
+
+# S3
+aws s3 ls
+
+aws s3 mb s3://guile-cli-bucket
+
+aws s3 cp in-class.yaml s3://guile-cli-bucket
+
+aws s3 ls s3://guile-cli-bucket
+
+aws s3 rm s3://guile-cli-bucket/in-class.yaml
+
+aws s3 rb s3://guile-cli-bucket        !!!!
+
+# EC2
+aws ec2 describe-instances
+##https://awscli.amazonaws.com/v2/documentation/api/latest/reference/ec2/describe-instances.html
+
+
+aws ec2 run-instances \
+   --image-id ami-033b95fb8079dc481 \
+   --count 1 \
+   --instance-type t2.micro \
+   --key-name KEY_NAME_HERE # put your key name 
+
+aws ec2 describe-instances \
+   --filters "Name = key-name, Values = KEY_NAME_HERE" # put your key name
+
+## https://awscli.amazonaws.com/v2/documentation/api/latest/reference/ec2/describe-instances.html
+
+## --dıy-run 
+## --dry-run | --no-dry-run (boolean)
+## Checks whether you have the required permissions for the action, without actually making the request, and provides an error response. If you have the required permissions, the error response is DryRunOperation . Otherwise, it is UnauthorizedOperation .
+
+
+aws ec2 describe-instances --query "Reservations[].Instances[].PublicIpAddress[]"
+
+aws ec2 describe-instances \
+   --filters "Name = key-name, Values = KEY_NAME_HERE" --query "Reservations[].Instances[].PublicIpAddress[]" # put your key name
+
+aws ec2 describe-instances \
+   --filters "Name = instance-type, Values = t2.micro" --query "Reservations[].Instances[].InstanceId[]"
+
+
+
+
+
+aws ec2 stop-instances --instance-ids INSTANCE_ID_HERE # put your instance id
+
+aws ec2 terminate-instances --instance-ids INSTANCE_ID_HERE # put your instance id
+
+# Working with the latest Amazon Linux AMI
+
+aws ssm get-parameters --names /aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2 --region us-east-1
+
+aws ssm get-parameters --names /aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2 --query 'Parameters[0].[Value]' --output text
+
+aws ec2 run-instances \
+   --image-id $(aws ssm get-parameters --names /aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2 --query 
+'Parameters[0].[Value]' --output text) \
+   --count 1 \
+   --instance-type t2.micro
+
+
+##
+aws ec2 run-instances \
+   --image-id ami-033b95fb8079dc481 \
+   --count 1 \
+   --instance-type t2.micro
+
+
+
+# Update AWS CLI Version 1 on Amazon Linux (comes default) to Version 2
+
+# Remove AWS CLI Version 1
+sudo yum remove awscli -y # pip uninstall awscli/pip3 uninstall awscli might also work depending on the image
+
+# Install AWS CLI Version 2
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip  #install "unzip" if not installed
+sudo ./aws/install
+
+# Update the path accordingly if needed
+export PATH=$PATH:/usr/local/bin/aws
+
+
+
+
+
